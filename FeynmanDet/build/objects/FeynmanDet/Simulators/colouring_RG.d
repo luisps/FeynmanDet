c65344d8cfd92ab1f356ef7934e8077582c5c8c1
Luis Paulo Santos <psantos@di.uminho.pt>
2025-08-10 20:24:48 +0100

    Removing .o from repo

--- FeynmanDet/build/objects/FeynmanDet/Simulators/colouring_RG.d
+++
@@ -1,5 +0,0 @@
-build/objects/FeynmanDet/Simulators/colouring_RG.o: \
-  FeynmanDet/Simulators/colouring_RG.cpp \
-  FeynmanDet/Simulators/colouring_RG.hpp \
-  FeynmanDet/Simulators/../BaseFunctionalty/circuit.h \
-  FeynmanDet/Simulators/colouring.hpp
