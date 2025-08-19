aa0e86bd5c386695d385c426f5cdadaf58710f5e
Luís Paulo Santos <psantos@di.uminho.pt>
2025-08-19 17:26:37 +0100

    Remove bjects from git

--- FeynmanDet/build/objects/FeynmanDet/Simulators/colouring_PB.d
+++
@@ -1,5 +0,0 @@
-build/objects/FeynmanDet/Simulators/colouring_PB.o: \
-  FeynmanDet/Simulators/colouring_PB.cpp \
-  FeynmanDet/Simulators/colouring_PB.hpp \
-  FeynmanDet/Simulators/../BaseFunctionalty/circuit.h \
-  FeynmanDet/Simulators/colouring.hpp
