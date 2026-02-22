42406b7812344562be6f20026da8d1047452e955
Luís Paulo Santos <psantos@di.uminho.pt>
2026-02-22 19:57:03 +0000

    simulate all OpenMP

--- FeynmanDet/FeynmanDet/Simulators/simulator_all.cpp
+++ FeynmanDet/FeynmanDet/Simulators/simulator_all.cpp
@@ -79,8 +79,8 @@ void simulate_all_paths (TCircuit *circuit, StateT init_state, StateT final_stat
         StateT ndxs0;
 
 #if defined(_OPENMP)
-//#define _COLLAPSE_D
-#define _SCRAMBLE
+#define _COLLAPSE_D
+//#define _SCRAMBLE
 #if defined(_COLLAPSE_D) or defined(_SCRAMBLE)
         int const D=3;
         // manually flatten the collapsed for loops
